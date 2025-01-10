af3d3c3981dec2c53485824d75fa735cb02b71c4
gasht1 <gebriegashaw7@gmail.com>
2025-01-10 16:33:53 +0300

    accesing input field

--- vite-projectt/src/components/Form.tsx
+++ vite-projectt/src/components/Form.tsx
@@ -1,15 +1,27 @@
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 
 const Form = () => {
+  const [person, setPerson] = useState({
+    name: "",
+    age: "",
+  });
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Submited");
+    console.log(person);
   };
   return (
     <form onSubmit={handleSubmit}>
       <div className=" flex flex-col px-2 mt-2">
         <label> Name</label>
-        <input className="" type="text" placeholder=" enter your first Name" />
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, name: event.target.value })
+          }
+          value={person.name}
+          className=""
+          type="text"
+          placeholder=" enter your first Name"
+        />
         <input className="" type="text" placeholder="   enter your Last Name" />
         <label> Name</label>
         <input className="" type="number" placeholder=" enter your age" />
