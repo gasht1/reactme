bdbac8fafdc61b8f104e806764f391a3f50071ad
gasht1 <gebriegashaw7@gmail.com>
2025-01-17 18:18:19 +0300

    select with user selected Categories

--- vite-projectt/src/App.tsx
+++ vite-projectt/src/App.tsx
@@ -7,22 +7,28 @@ import Expense from "./expense_traker/components/Expense";
 import ExpenseForm from "./expense_traker/components/ExpenseForm";
 
 function App() {
-  const [selectCatagorey, setselectCatagorey] = useState();
+  const [selectCatagorey, setselectCatagorey] = useState("");
+
   const [expenseData, setexpenseData] = useState([
     { id: 1, description: " aaaa", amount: 20, category: "utility" },
     { id: 2, description: " bbbb", amount: 30, category: "utility" },
     { id: 3, description: " cccc", amount: 40, category: "utility" },
     { id: 4, description: " ddddd", amount: 50, category: "utility" },
   ]);
+  const visibleCategory = selectCatagorey
+    ? expenseData.filter((e) => e.category === selectCatagorey)
+    : expenseData;
   //const [Alertvisible, setAlertvisible] = useState(false);
   return (
     <div>
       <div>
-        <ExpenseForm onSelectCategory={(category) => console.log(category)} />
+        <ExpenseForm
+          onSelectCategory={(category) => setselectCatagorey(category)}
+        />
       </div>
       {
         <Expense
-          expenses={expenseData}
+          expenses={visibleCategory}
           ondelete={(id) =>
             setexpenseData(expenseData.filter((e) => e.id !== id))
           }
